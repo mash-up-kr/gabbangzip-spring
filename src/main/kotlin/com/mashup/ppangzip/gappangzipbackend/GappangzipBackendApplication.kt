560e0ed4fb4a9116a042e5dde654a4e0f9cc5953
Jaemin.Park <once29@naver.com>
2024-06-04 21:34:05 +0900

    feat: Initialize project

---
+++ src/main/kotlin/com/mashup/ppangzip/gappangzipbackend/GappangzipBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.mashup.ppangzip.gappangzipbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GappangzipBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<GappangzipBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/mashup/ppangzip/gappangzipbackend/GappangzipBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mashup.ppangzip.gappangzipbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GappangzipBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

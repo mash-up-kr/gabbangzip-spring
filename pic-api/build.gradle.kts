689c024d0ff6480f4fa4ffbc5966bedb4d709221
210-reverof <sheltonwon@gmail.com>
2024-06-07 16:32:12 +0900

    chore: Add dependencies with settings

--- pic-api/build.gradle.kts
+++ pic-api/build.gradle.kts
@@ -6,10 +6,17 @@ tasks.jar {
     enabled = false
 }
 
+val jjwtVersion = "0.11.5"
+
 dependencies {
     implementation(project(":pic-common"))
     implementation(project(":pic-domain"))
     implementation(project(":pic-external"))
+
+    // jwt
+    implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:$jjwtVersion")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jjwtVersion")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
--- pic-domain/build.gradle.kts
+++ pic-domain/build.gradle.kts
@@ -1,5 +1,10 @@
+val mysqlConnectorVersion = "8.0.32"
+
 dependencies {
     implementation(project(":pic-common"))
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.mysql:mysql-connector-j:$mysqlConnectorVersion")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }

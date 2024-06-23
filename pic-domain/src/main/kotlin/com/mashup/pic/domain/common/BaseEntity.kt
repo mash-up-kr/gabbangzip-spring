d619dbc38e0dade7dccd83c7bc031a15cb0cd6e3
Jaemin.Park <once29@naver.com>
2024-06-24 00:06:55 +0900

    fix: Fix broken workflow

--- pic-domain/src/main/kotlin/com/mashup/pic/domain/common/BaseEntity.kt
+++ pic-domain/src/main/kotlin/com/mashup/pic/domain/common/BaseEntity.kt
@@ -4,9 +4,9 @@ import jakarta.persistence.Column
 import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
 import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime

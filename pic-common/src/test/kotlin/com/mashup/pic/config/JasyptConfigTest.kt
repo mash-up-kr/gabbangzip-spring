b173808f615d7b3924a957ede774add8a889ee70
Jake <jake@wrtn.io>
2024-06-23 18:07:30 +0900

    feat: apply jasypt

--- pic-common/src/test/kotlin/com/mashup/pic/config/JasyptConfigTest.kt
+++ pic-common/src/test/kotlin/com/mashup/pic/config/JasyptConfigTest.kt
@@ -11,13 +11,13 @@ import org.springframework.boot.test.context.SpringBootTest
 /**
  * Run > Edit Configurations > Configuration > Environment variables > 'JASYPT_ENCRYPTOR_PASSWORD={암호화키}' 입력
  */
-//@Disabled("암호화, 복호화 결과 확인을 위한 테스트이므로 비활성화")
+@Disabled("암호화, 복호화 결과 확인을 위한 테스트이므로 비활성화")
 @SpringBootTest(classes = [JasyptConfig::class])
 class JasyptConfigTest(
     private val stringEncryptor: StringEncryptor
 ) : SharedContext() {
 
-    private val plainText: String = "eojngnwegjnweognoi;uewgnn"
+    private val plainText: String = "test"
 
     @Test
     fun execute() {

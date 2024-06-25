18add9a39d55a570d7d8319d1efd42c66a354db8
210-reverof <sheltonwon@gmail.com>
2024-06-26 01:17:42 +0900

    chore: Update type var->val

--- pic-domain/src/main/kotlin/com/mashup/pic/domain/auth/RefreshToken.kt
+++ pic-domain/src/main/kotlin/com/mashup/pic/domain/auth/RefreshToken.kt
@@ -6,6 +6,6 @@ import org.springframework.data.redis.core.RedisHash
 @RedisHash(value = "refresh_token", timeToLive = 30L * 24 * 60 * 60 * 1000)
 class RefreshToken(
     @Id
-    var refreshToken: String,
-    var userId: Long,
+    val refreshToken: String,
+    val userId: Long,
 )

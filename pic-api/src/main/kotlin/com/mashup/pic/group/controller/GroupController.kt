63246efb4a1c1f14f447b110ca86b7e92d2a689a
Jaemin.Park <once29@naver.com>
2024-06-28 00:22:56 +0900

    feat: Implement mock api of creating group

---
+++ pic-api/src/main/kotlin/com/mashup/pic/group/controller/GroupController.kt
@@ -0,0 +1,30 @@
+package com.mashup.pic.group.controller
+
+import com.mashup.pic.common.ApiResponse
+import com.mashup.pic.group.applicationservice.GroupApplicationService
+import com.mashup.pic.group.applicationservice.dto.CreateGroupResponse
+import com.mashup.pic.group.controller.dto.CreateGroupRequest
+import com.mashup.pic.group.controller.dto.toServiceRequest
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Tag(name = "그룹")
+@RestController
+@RequestMapping("/api/v1/group")
+class GroupController(private val groupApplicationService: GroupApplicationService) {
+
+    @Operation(summary = "그룹 만들기 API", security = [SecurityRequirement(name = "Authorization")])
+    @PostMapping
+    fun create(@RequestBody request: CreateGroupRequest): ApiResponse<CreateGroupResponse> {
+        // TODO: Use authorized user id
+        val userId = 0L
+        return ApiResponse.success(
+            groupApplicationService.create(request.toServiceRequest(userId))
+        )
+    }
+}
---
+++ pic-api/src/main/kotlin/com/mashup/pic/group/controller/dto/GroupRequestDto.kt
@@ -0,0 +1,27 @@
+package com.mashup.pic.group.controller.dto
+
+import com.mashup.pic.group.applicationservice.dto.CreateGroupServiceRequest
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
+
+@Schema(description = "[Request] 그룹 생성")
+data class CreateGroupRequest(
+    @Schema(description = "그룹 이름")
+    @field:NotBlank(message = "그룹 이름은 공백이 아니어야 합니다.")
+    val groupName: String,
+
+    @Schema(description = "그룹 키워드")
+    val keyword: GroupKeyword,
+
+    @Schema(description = "그룹 이미지 URL")
+    @field:NotBlank(message = "그룹 이미지 URL은 공백이 아니어야 합니다.")
+    val groupImageUrl: String? = null,
+)
+
+fun CreateGroupRequest.toServiceRequest(userId: Long): CreateGroupServiceRequest =
+    CreateGroupServiceRequest(userId, groupName, keyword, groupImageUrl)
+
+// TODO: Move to domain module when developing persistence logic
+enum class GroupKeyword {
+    SCHOOL, COMPANY, CREW, NETWORK, EXERCISE, HOBBY, LITTLE_MOIM
+}

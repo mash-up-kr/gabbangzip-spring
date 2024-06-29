3e71ebb22c93fd49fc5d8b58f784ce747191c615
210-reverof <sheltonwon@gmail.com>
2024-06-29 19:26:55 +0900

    feat: Add group join entity

--- pic-domain/src/main/kotlin/com/mashup/pic/domain/group/GroupJoin.kt
+++ pic-domain/src/main/kotlin/com/mashup/pic/domain/group/GroupJoin.kt
@@ -1,23 +1,22 @@
 package com.mashup.pic.domain.group
 
 import com.mashup.pic.domain.common.BaseEntity
-import jakarta.persistence.Column
+import com.mashup.pic.domain.user.User
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
-import jakarta.persistence.ManyToOne
 import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import org.hibernate.annotations.SQLDelete
 import org.hibernate.annotations.SQLRestriction
 
 @Entity
 @SQLDelete(sql = "UPDATE group SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
 @SQLRestriction("deleted_at is NULL")
-class Group(
-    @Column(nullable = false)
-    val name: String,
+class GroupJoin (
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "keyword_id", unique = true)
-    val keyword: Keyword,
-    @Column(nullable = false)
-    val imageUrl: String
+    @JoinColumn(name = "user_id", unique = true)
+    val user: User,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", unique = true)
+    val group: Group
 ) : BaseEntity()

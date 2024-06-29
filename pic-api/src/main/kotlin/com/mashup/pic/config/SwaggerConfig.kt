b06b4f38eb9c2071613376caf1be8a508792322d
210-reverof <sheltonwon@gmail.com>
2024-06-30 04:32:10 +0900

    chore: Reformat with kt lint

--- pic-api/src/main/kotlin/com/mashup/pic/config/SwaggerConfig.kt
+++ pic-api/src/main/kotlin/com/mashup/pic/config/SwaggerConfig.kt
@@ -1,10 +1,8 @@
 package com.mashup.pic.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.mashup.pic.common.ErrorResponse
 import com.mashup.pic.common.exception.PicExceptionType
-import io.swagger.v3.core.jackson.ModelResolver
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.models.Components

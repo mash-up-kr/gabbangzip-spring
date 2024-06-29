4c040bdaaefb25bdae5c7248c774dbfe6e43ac38
bbeomgeun <miki30808@gmail.com>
2024-06-29 20:05:03 +0900

    refactor: imageUrlList로 변경, 검증 추가

--- pic-api/src/main/kotlin/com/mashup/pic/vote/controller/dto/UploadMyPicRequest.kt
+++ pic-api/src/main/kotlin/com/mashup/pic/vote/controller/dto/UploadMyPicRequest.kt
@@ -1,6 +1,9 @@
 package com.mashup.pic.vote.controller.dto
 
+import jakarta.validation.constraints.Size
+
 data class UploadMyPicRequest(
     val eventId: String,
-    val picImageUrl: String,
+    @field:Size(min = 1, max = 4)
+    val picImageUrls: List<String>
 )

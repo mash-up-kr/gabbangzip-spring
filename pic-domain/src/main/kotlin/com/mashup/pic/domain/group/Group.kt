05e7c7377d4fd0650c727cf11f0488ba1c89b4fa
210-reverof <sheltonwon@gmail.com>
2024-06-30 03:56:07 +0900

    fix: Rename group table

--- pic-domain/src/main/kotlin/com/mashup/pic/domain/group/Group.kt
+++ pic-domain/src/main/kotlin/com/mashup/pic/domain/group/Group.kt
@@ -6,11 +6,13 @@ import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 import org.hibernate.annotations.SQLDelete
 import org.hibernate.annotations.SQLRestriction
 
 @Entity
-@SQLDelete(sql = "UPDATE group SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@Table(name="croup")
+@SQLDelete(sql = "UPDATE croup SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
 @SQLRestriction("deleted_at is NULL")
 class Group(
     @Column(nullable = false)

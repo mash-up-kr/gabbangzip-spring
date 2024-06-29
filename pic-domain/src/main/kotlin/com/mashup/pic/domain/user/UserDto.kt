f8166cfa8f3d1e87a1dac2196444c85ce9a21a71
210-reverof <sheltonwon@gmail.com>
2024-06-29 16:54:09 +0900

    fix: Add Provider field

--- pic-domain/src/main/kotlin/com/mashup/pic/domain/user/UserDto.kt
+++ pic-domain/src/main/kotlin/com/mashup/pic/domain/user/UserDto.kt
@@ -3,6 +3,7 @@ package com.mashup.pic.domain.user
 data class UserDto(
     val id: Long,
     val oAuthId: Long,
+    val provider: String,
     val nickname: String,
     val profileImage: String,
     val roles: Set<UserRole>
@@ -12,6 +13,7 @@ fun User.toUserDto(): UserDto {
     return UserDto(
         id = this.id,
         oAuthId = this.oAuthId,
+        provider = this.provider.name,
         nickname = this.nickname,
         profileImage = this.profileImage,
         roles = this.roles

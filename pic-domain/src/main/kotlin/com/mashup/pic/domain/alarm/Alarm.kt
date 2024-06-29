45218b4c819eb5652dd81d23705fda2ec526b976
210-reverof <sheltonwon@gmail.com>
2024-06-30 00:29:04 +0900

    feat: Create Alarm Entity

---
+++ pic-domain/src/main/kotlin/com/mashup/pic/domain/alarm/Alarm.kt
@@ -0,0 +1,24 @@
+package com.mashup.pic.domain.alarm
+
+import com.mashup.pic.domain.common.BaseEntity
+import com.mashup.pic.domain.user.User
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.SQLRestriction
+
+@Entity
+@SQLDelete(sql = "UPDATE alarm SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@SQLRestriction("deleted_at is NULL")
+class Alarm (
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    val user: User,
+    @Column(nullable = false)
+    val message: String,
+    @Column(nullable = false)
+    val read: Boolean = false
+) : BaseEntity()

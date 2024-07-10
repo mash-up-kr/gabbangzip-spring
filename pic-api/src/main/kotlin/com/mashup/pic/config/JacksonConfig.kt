6383bf2fdcd2cef4c36f9b15918fdffda570668f
Jaemin.Park <once29@naver.com>
2024-07-10 20:20:21 +0900

    fix: Fix configurating jackson library logic

--- pic-api/src/main/kotlin/com/mashup/pic/config/JacksonConfig.kt
+++ pic-api/src/main/kotlin/com/mashup/pic/config/JacksonConfig.kt
@@ -1,36 +1,16 @@
 package com.mashup.pic.config
 
-import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer
+import com.mashup.pic.jackson.PicObjectMapperBuilderCustomizer
 import io.swagger.v3.core.jackson.ModelResolver
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
 
 @Configuration
 class JacksonConfig {
     @Bean
-    fun jackson2ObjectMapperBuilder(customizers: List<Jackson2ObjectMapperBuilderCustomizer>): Jackson2ObjectMapperBuilder {
-        val builder =
-            Jackson2ObjectMapperBuilder()
-                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .featuresToDisable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-                .serializerByType(LocalDateTime::class.java, LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
-                .serializerByType(LocalDate::class.java, LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE))
-                .serializerByType(LocalTime::class.java, LocalTimeSerializer(DateTimeFormatter.ISO_LOCAL_TIME))
-                .propertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
-        customizers.forEach { customizer -> customizer.customize(builder) }
-        return builder
+    fun picObjectMapperBuilderCustomizer(): PicObjectMapperBuilderCustomizer {
+        return PicObjectMapperBuilderCustomizer()
     }
 
     @Bean

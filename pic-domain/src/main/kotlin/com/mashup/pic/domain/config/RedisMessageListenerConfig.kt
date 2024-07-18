2cd6f07802bd3f704b94340d6b336ae64681c762
bbeomgeun <miki30808@gmail.com>
2024-07-19 03:28:42 +0900

    feat: RedisMessageListener config 추가

---
+++ pic-domain/src/main/kotlin/com/mashup/pic/domain/config/RedisMessageListenerConfig.kt
@@ -0,0 +1,39 @@
+package com.mashup.pic.domain.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.MessageListener
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisKeyValueAdapter
+import org.springframework.data.redis.listener.ChannelTopic
+import org.springframework.data.redis.listener.RedisMessageListenerContainer
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+
+@Configuration
+@EnableRedisRepositories(
+    enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP
+)
+class RedisMessageListenerConfig {
+    @Bean
+    fun messageListenerAdaptor(
+        redisMessageListener: MessageListener
+    ): MessageListenerAdapter {
+        return MessageListenerAdapter(redisMessageListener)
+    }
+
+    @Bean
+    fun redisContainer(
+        redisConnectionFactory: RedisConnectionFactory,
+        messageListenerAdapter: MessageListenerAdapter
+    ): RedisMessageListenerContainer {
+        return RedisMessageListenerContainer().apply {
+            setConnectionFactory(redisConnectionFactory)
+            addMessageListener(messageListenerAdapter, ChannelTopic(EXPIRE_EVENT_TOPIC))
+        }
+    }
+
+    companion object {
+        private const val EXPIRE_EVENT_TOPIC = "__keyevent@0__:expired"
+    }
+}

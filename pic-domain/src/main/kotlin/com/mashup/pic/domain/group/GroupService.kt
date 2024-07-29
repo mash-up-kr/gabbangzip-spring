6562da647594ddd5907aaa5663c19cd18a813d1b
210-reverof <sheltonwon@gmail.com>
2024-07-30 03:30:56 +0900

    refactor: Add validating process

--- pic-domain/src/main/kotlin/com/mashup/pic/domain/group/GroupService.kt
+++ pic-domain/src/main/kotlin/com/mashup/pic/domain/group/GroupService.kt
@@ -2,6 +2,7 @@ package com.mashup.pic.domain.group
 
 import com.mashup.pic.common.exception.PicException
 import com.mashup.pic.common.exception.PicExceptionType
+import com.mashup.pic.domain.user.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional(readOnly = true)
 class GroupService(
     private val groupRepository: GroupRepository,
+    private val userRepository: UserRepository,
     private val groupJoinRepository: GroupJoinRepository
 ) {
     @Transactional
@@ -17,7 +19,8 @@ class GroupService(
         keyword: GroupKeyword,
         imageUrl: String
     ): GroupDto {
-        return groupRepository.save(Group(name, keyword, imageUrl)).toDto()
+        val group = Group(name, keyword, imageUrl)
+        return groupRepository.save(group).toDto()
     }
 
     @Transactional
@@ -25,22 +28,60 @@ class GroupService(
         userId: Long,
         groupId: Long
     ): GroupJoinDto {
+        validateUser(userId)
+        validateGroup(groupId)
+        validateGroupCapacity(groupId)
+        checkAlreadyJoined(userId, groupId)
+
+        return groupJoinRepository.save(GroupJoin(userId, groupId)).toDto()
+    }
+
+    private fun validateUser(userId: Long) {
+        if (!checkUserExists(userId)) {
+            throw PicException.of(
+                type = PicExceptionType.NOT_EXIST,
+                message = "$userId 에 해당하는 유저를 찾을 수 없습니다."
+            )
+        }
+    }
+
+    private fun validateGroup(groupId: Long) {
+        if (!checkGroupExists(groupId)) {
+            throw PicException.of(
+                type = PicExceptionType.NOT_EXIST,
+                message = "$groupId 에 해당하는 그룹을 찾을 수 없습니다."
+            )
+        }
+    }
+
+    private fun validateGroupCapacity(groupId: Long) {
         if (isGroupFull(groupId)) {
             throw PicException.of(PicExceptionType.ARGUMENT_NOT_VALID, "인원 초과")
         }
+    }
 
-        if (isAlreadyJoined(userId, groupId)) {
+    private fun checkAlreadyJoined(
+        userId: Long,
+        groupId: Long
+    ) {
+        if (hadAlreadyJoined(userId, groupId)) {
             throw PicException.of(PicExceptionType.ARGUMENT_NOT_VALID, "이미 참여")
         }
+    }
 
-        return groupJoinRepository.save(GroupJoin(userId, groupId)).toDto()
+    private fun checkUserExists(userId: Long): Boolean {
+        return userRepository.existsById(userId)
+    }
+
+    private fun checkGroupExists(groupId: Long): Boolean {
+        return groupRepository.existsById(groupId)
     }
 
     private fun isGroupFull(groupId: Long): Boolean {
         return groupJoinRepository.findAllByGroupId(groupId).size >= GROUP_MEMBER_MAX_COUNT
     }
 
-    private fun isAlreadyJoined(
+    private fun hadAlreadyJoined(
         userId: Long,
         groupId: Long
     ): Boolean {

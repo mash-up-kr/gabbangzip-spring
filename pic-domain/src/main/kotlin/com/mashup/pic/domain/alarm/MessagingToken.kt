7543ac8288bee5ec1a2e5cc2ee35253ab09e9262
210-reverof <sheltonwon@gmail.com>
2024-07-30 00:37:21 +0900

    feat: Create MessagingToken Entitu

---
+++ pic-domain/src/main/kotlin/com/mashup/pic/domain/alarm/MessagingToken.kt
@@ -0,0 +1,23 @@
+package com.mashup.pic.domain.alarm
+
+import com.mashup.pic.domain.common.BaseEntity
+import com.mashup.pic.domain.user.User
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.SQLRestriction
+
+@Entity
+@SQLDelete(sql = "UPDATE croup SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@SQLRestriction("deleted_at is NULL")
+class MessagingToken(
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    val user: User,
+    @Column(nullable = false)
+    val token: String
+) : BaseEntity()
+

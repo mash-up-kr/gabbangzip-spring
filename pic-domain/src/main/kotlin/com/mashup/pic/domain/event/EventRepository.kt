b682439f4dee4038d357c6d55c693f1912640eea
210-reverof <sheltonwon@gmail.com>
2024-08-05 20:01:25 +0900

    fix: Update event method

--- pic-domain/src/main/kotlin/com/mashup/pic/domain/event/EventRepository.kt
+++ pic-domain/src/main/kotlin/com/mashup/pic/domain/event/EventRepository.kt
@@ -3,9 +3,9 @@ package com.mashup.pic.domain.event
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface EventRepository : JpaRepository<Event, Long> {
-    fun findTopByGroupIdOrderByDateDesc(groupId: Long): Event?
-
-    fun findTopByGroupIdOrderByDateAsc(groupId: Long): Event?
+    fun findTopByGroupIdOrderByIdDesc(groupId: Long): Event?
 
     fun findAllByGroupIdOrderByIdDesc(groupId: Long): List<Event>
+
+    fun findAllByGroupId(groupId: Long): List<Event>
 }
